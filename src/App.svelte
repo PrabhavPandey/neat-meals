4203897f0ad143b287accbc6f5f259971d858581
Prabhav <prabhavp380@gmail.com>
2022-05-20 00:13:15 +0530

    some cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,23 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <section>
+    <h1>YOO </h1>
+  </section>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  section {
+    padding: 0;
+    margin: 0;
+    height: 100vh;
+    background-color: rgb(80, 80, 162);
+    color: white;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    margin: 0;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
