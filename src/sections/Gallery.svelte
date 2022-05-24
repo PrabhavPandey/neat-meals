488b8c825dd5bafedf8382b927e8040d64bd6449
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-05-24 14:56:44 +0530

    feat(Gallery) - image addition --failed

--- src/sections/Gallery.svelte
+++ src/sections/Gallery.svelte
@@ -5,11 +5,11 @@
     <section class="gallery-section site-container">
         <h1>Gallery</h1>
             <div class="gallery-container">
-                <div style="background-image:url('https://images.unsplash.com/photo-1558981359-219d6364c9c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80')" class="Box card-tall"></div>
-                <div style="background-image:url('https://images.unsplash.com/photo-1583585635793-0e1894c169bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=913&q=80')" class="Box"></div>
-                <div style="background-image:url('https://images.unsplash.com/photo-1583531172005-814191b8b6c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80')" class="Box card-wide"></div>
-                <div style="background-image:url('https://images.unsplash.com/photo-1583426573939-97d09302d76a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=968&q=80')" class="Box card-wide"></div>
-                <div style="background-image:url('https://images.unsplash.com/photo-1583532452513-a02186582ccd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80')" class="Box card-tall-reverse"></div>
+                <div style="background-image:url('assets/InstaImg1.jpeg')" class="Box card-tall"></div>
+                <div style="background-image:url('assets/InstaImg2.jpeg')" class="Box"></div>
+                <div style="background-image:url('assets/InstaImg3.jpeg')" class="Box card-wide"></div>
+                <div style="background-image:url('assets/InstaImg4.jpeg')" class="Box card-wide"></div>
+                <div style="background-image:url('assets/InstaImg5.jpeg')" class="Box card-tall-reverse"></div>
             </div>
     </section>
 </main>
@@ -38,6 +38,7 @@
   }
 
   .Box {
+    background-size: cover;
     filter: brightness(40%);
     transition: all 0.5s;
   }
