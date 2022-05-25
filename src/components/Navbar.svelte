9f9ab1559b4bc64d23a6907a89280f5738f41278
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-05-25 11:52:34 +0530

    Btn animation state added

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,19 +1,29 @@
-<script></script>
+<script>
+    let isActive = false;
+    const setState = () => { isActive = !isActive}
+</script>
 
 <main>
-    <nav class="site-container">
-        <ul class="navigation-primary">
-          <li><a class="nav-primary-item" href="#services-section">Services</a></li>
-          <!-- <li><a class="nav-primary-item" href="#gallery-section">Gallery</a></li> -->
-          <li><a class="nav-primary-item" href="#article-section">Articles</a></li>
-          <li><a class="nav-primary-item" href="#">About Us</a></li>
-        </ul>
-     
-        <ul class="navigation-soc">
-          <li class="nav-soc-item"><img src="assets/linkedinLogo.svg" alt=""></li>
-          <li class="nav-soc-item"><img src="assets/mailLogo.svg" alt=""></li>
-        </ul>
-      </nav>
+    <header>
+        <nav class="pr-nav site-container">
+            <ul class="navigation-primary">
+              <li><a class="nav-primary-item" href="#services-section">Services</a></li>
+              <!-- <li><a class="nav-primary-item" href="#gallery-section">Gallery</a></li> -->
+              <li><a class="nav-primary-item" href="#article-section">Articles</a></li>
+              <li><a class="nav-primary-item" href="#">About Us</a></li>
+            </ul>
+         
+            <ul class="navigation-soc">
+              <li class="nav-soc-item"><img src="assets/linkedinLogo.svg" alt=""></li>
+              <li class="nav-soc-item"><img src="assets/mailLogo.svg" alt=""></li>
+            </ul>
+        </nav>
+        <div class="mobile-navHolder">
+            <button on:click={setState} class="hamburger" class:is-active={isActive}>
+                <div class="bar"></div>
+            </button>
+        </div>
+    </header>
 </main>
 
 <style>
@@ -65,5 +75,64 @@
   .nav-soc-item img {
     width: 23px;
   }
+
+  
+.hamburger {
+	position: relative;
+	display: block;
+	width: 35px;
+	cursor: pointer;
+	
+	appearance: none;
+	background: none;
+	outline: none;
+	border: none;
+}
+
+.hamburger .bar, .hamburger:after, .hamburger:before {
+	content: '';
+	display: block;
+	width: 100%;
+	height: 3px;
+	background-color: #FFF;
+	margin: 6px 0px;
+	transition: 0.4s;
+}
+
+.hamburger.is-active:before {
+	transform: rotate(-45deg) translate(-8px, 6px);
+	height: 5px;
+}
+
+.hamburger.is-active:after {
+	transform: rotate(45deg) translate(-9px, -8px);
+	height: 5px;
+}
+
+.hamburger.is-active .bar {
+	opacity: 0;
+}
+
+@media only screen and (max-width: 768px) {
+    .navigation-primary {
+        display: none;
+    }
+
+    .navigation-soc {
+        display: none;
+    }
+}
+
+@media only screen and (min-width: 768px) {
+	.hamburger {
+		display: none;
+        left: 0;
+	}
+}
+
+.mobile-navHolder {
+    display: flex;
+    justify-content: flex-end;
+}
 
 </style>
