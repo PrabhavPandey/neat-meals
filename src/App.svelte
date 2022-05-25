d8cee6a014cef8d7e94f96fa74a99889ba19efb6
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-05-25 18:08:45 +0530

    Routing added

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,16 @@
-<script>
+<script lang="ts">
   import Home from './Pages/Home.svelte';
   import Aboutus from './Pages/Aboutus.svelte';
+  import Router from 'svelte-spa-router'
+  //Route defination (single page mapping only)
+  const Pages = {
+    "/": Home,
+    "/about": Aboutus
+  }
 </script>
 
 <main>
-  <!-- <Aboutus /> -->
-  <Home />
+  <Router routes={Pages}/>
 </main>
 
 <style></style>
