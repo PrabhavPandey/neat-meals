202b20027594f2dc58f6edf338909920911238d3
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-05-25 22:24:59 +0530

    Re deploy

--- src/Pages/Aboutus.svelte
+++ src/Pages/Aboutus.svelte
@@ -10,7 +10,7 @@
         <h1>About Us</h1>
         <p>Neat Meals Company is a luxury meal service that provides nutrient-rich and globally inspired meals that are planned by nutritionists and designed by our seasoned chefs. We aspire to provide our clients with a flavorsome way to accomplish their health requirements. <br /><br /> Our chefs are passionate about cooking and are extensively trained to create culinary masterpieces. The process of creating a dish right from the farm to the plate is a journey and thus we handpick our ingredients to ensure optimum quality. We provide delectable menus to our clients WHENEVER and WHEREVER they need . The menu never goes dull with us!</p>
       </container>
-      <img class="section-img" src="assets/Chars.svg" />
+      <img class="section-img" src="assets/Chars.svg" alt="jk"/>
     </article>
   </main>
 
