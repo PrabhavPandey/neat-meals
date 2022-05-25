f62db8bb07a9012fac8f605fc36e84f1f17238a1
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-05-25 10:33:46 +0530

    mobile View compelete

--- src/Pages/Aboutus.svelte
+++ src/Pages/Aboutus.svelte
@@ -1,8 +1,10 @@
 <script>
+    import Navbar from "../components/Navbar.svelte"
 </script>
 
 <main>
-    <article>
+    <Navbar />
+    <article class="site-container">
       <container>
         <h1>About Us</h1>
         <p>Neat Meals Company is a luxury meal service that provides nutrient-rich and globally inspired meals that are planned by nutritionists and designed by our seasoned chefs. We aspire to provide our clients with a flavorsome way to accomplish their health requirements. <br /><br /> Our chefs are passionate about cooking and are extensively trained to create culinary masterpieces. The process of creating a dish right from the farm to the plate is a journey and thus we handpick our ingredients to ensure optimum quality. We provide delectable menus to our clients WHENEVER and WHEREVER they need . The menu never goes dull with us!</p>
@@ -15,14 +17,15 @@
 article {
     display: flex;
     flex-direction: row-reverse;
-    align-items: center;
+    align-items: flex-end;
     gap: 10vw;
     width: 74vw;
     margin: 0 auto;
 }
 
 h1 {
-  font-size: 550%;
+  color: var(--primary);
+  font-size: 80px;
   font-weight: 600;
   margin: 0;
 }
@@ -34,5 +37,30 @@ p {
 
 .section-img {
     width: 400px;
+}
+
+article {
+    padding-top: 80px;
+}
+
+@media only screen and (max-width: 1024px) {
+    .section-img {
+        width: 300px;
+    }
+
+    p {
+        font-size: 12px;
+        text-align: center;
+    }
+
+    article {
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h1 {
+        font-size: 30px;
+        text-align: center;
+    }
 }
 </style>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,69 @@
+<script></script>
+
+<main>
+    <nav class="site-container">
+        <ul class="navigation-primary">
+          <li><a class="nav-primary-item" href="#services-section">Services</a></li>
+          <!-- <li><a class="nav-primary-item" href="#gallery-section">Gallery</a></li> -->
+          <li><a class="nav-primary-item" href="#article-section">Articles</a></li>
+          <li><a class="nav-primary-item" href="#">About Us</a></li>
+        </ul>
+     
+        <ul class="navigation-soc">
+          <li class="nav-soc-item"><img src="assets/linkedinLogo.svg" alt=""></li>
+          <li class="nav-soc-item"><img src="assets/mailLogo.svg" alt=""></li>
+        </ul>
+      </nav>
+</main>
+
+<style>
+    nav {
+    width: 92vw;
+    padding-top: 35px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .navigation-primary {
+    display: flex;
+    cursor: pointer;
+    list-style: none;
+    gap: 70px;
+    font-size: 15px;
+    justify-content: space-between;
+  }
+
+  .nav-primary-item {
+    text-decoration: none;
+    transition: all 0.3s;
+    color: var(--secondary);
+    filter: opacity(60%);
+  }
+
+  .nav-primary-item:hover {
+    /* color: var(--accent); */
+    filter: opacity(100%);
+    transition: all 0.3s;
+    font-weight: 500;
+  }
+
+  .navigation-soc{
+    margin-right: 3vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 80px;
+    cursor: pointer;
+    list-style: none;
+  }
+
+  .navigation-soc img {
+    width: 15px;
+  }
+
+  .nav-soc-item img {
+    width: 23px;
+  }
+
+</style>
