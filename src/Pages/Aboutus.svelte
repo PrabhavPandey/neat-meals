b588c56973b75a3444b1f579f7cdd9e887fb5666
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-05-26 17:46:01 +0530

    animations added

--- src/Pages/Aboutus.svelte
+++ src/Pages/Aboutus.svelte
@@ -1,11 +1,22 @@
 <script>
     import Navbar from "../components/Navbar.svelte"
     let pageState = true
+    let fadeState = 'faddedState site-container'
+
+    const fadeIn = () => { 
+      fadeState = 'nav-container site-container'
+      console.log(fadeState) 
+    }
+
+    setTimeout(() => {
+        fadeIn()
+    }, 600);
+
 </script>
 
 <main>
     <Navbar pageLocation={pageState}/>
-    <article class="site-container">
+    <article class={fadeState}>
       <container>
         <h1>About Us</h1>
         <p>Neat Meals Company is a luxury meal service that provides nutrient-rich and globally inspired meals that are planned by nutritionists and designed by our seasoned chefs. We aspire to provide our clients with a flavorsome way to accomplish their health requirements. <br /><br /> Our chefs are passionate about cooking and are extensively trained to create culinary masterpieces. The process of creating a dish right from the farm to the plate is a journey and thus we handpick our ingredients to ensure optimum quality. We provide delectable menus to our clients WHENEVER and WHEREVER they need . The menu never goes dull with us!</p>
@@ -15,6 +26,17 @@
   </main>
 
 <style>
+
+.faddedState {
+    opacity: 0;
+    transform: translateY(-10%);
+    transition: all 1s;
+}
+.pg-container {
+    opacity: 1;
+    transform: translateY(0);
+    transition: all 1s;
+}
 article {
     display: flex;
     flex-direction: row-reverse;
