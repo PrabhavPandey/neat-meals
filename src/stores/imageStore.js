2d43a51ba4f98734917e1d82828e65b35a728eb1
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-06-11 14:57:45 +0530

    seo stuff added

--- src/stores/imageStore.js
+++ src/stores/imageStore.js
@@ -3,51 +3,51 @@ export const imageStore = writable([
   {
     id: 1,
     src: "./assets/InstaImg1.jpeg",
-    alt: " background",
+    alt: "Chicken Steak Burger",
   },
   {
     id: 2,
     src: "./assets/InstaImg2.jpeg",
-    alt: " background",
+    alt: "Burmese khow suey",
   },
   {
     id: 3,
     src: "./assets/InstaImg3.jpeg",
-    alt: " background",
+    alt: "Gluten free chocolate cookies",
   },
   {
     id: 4,
     src: "./assets/InstaImg4.jpeg",
-    alt: " background",
+    alt: "Grilled Pesto Chicken with wilted herbed spinach and mushroom",
   },
   {
     id: 5,
     src: "./assets/InstaImg5.jpeg",
-    alt: " background",
+    alt: "Mexican mango salsa",
   },
   {
     id: 6,
     src: "./assets/InstaImg6.jpeg",
-    alt: " background",
+    alt: "Vanilla gluten free pancakes topped with poached berries",
   },
   {
     id: 7,
     src: "./assets/InstaImg7.jpeg",
-    alt: " background",
+    alt: "Vegan soba noodles bowl",
   },
   {
     id: 8,
     src: "./assets/InstaImg8.jpeg",
-    alt: " background",
+    alt: "Gluten free paleo pancakes",
   },
   {
     id: 9,
     src: "./assets/InstaImg9.jpeg",
-    alt: " background",
+    alt: "Teriyaki bowl",
   },
   {
     id: 10,
     src: "./assets/InstaImg10.jpeg",
-    alt: " background",
+    alt: "Farm house salad",
   },
 ]);
